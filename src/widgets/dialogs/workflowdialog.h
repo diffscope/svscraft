2ca75080a5d0922fd026636ad157a514ad66b2c2
Sine Striker <trueful@163.com>
2024-02-08 02:58:57 +0800

    Add dialogs

---
+++ src/widgets/dialogs/workflowdialog.h
@@ -0,0 +1,44 @@
+#ifndef CWORKFLOWDIALOG_H
+#define CWORKFLOWDIALOG_H
+
+#include <QDialog>
+
+#include <SVSCraftWidgets/workflowpage.h>
+
+namespace SVS {
+
+    class WorkflowDialogPrivate;
+
+    class SVSCRAFT_WIDGETS_EXPORT WorkflowDialog : public QDialog {
+        Q_OBJECT
+        Q_DECLARE_PRIVATE(WorkflowDialog)
+    public:
+        explicit WorkflowDialog(QWidget *parent = nullptr);
+        ~WorkflowDialog();
+
+    public:
+        WorkflowPage *page() const;
+        WorkflowPage *takePage();
+        void setPage(WorkflowPage *w);
+
+    protected:
+        virtual bool prev();
+        virtual bool next();
+        virtual bool finish();
+
+    Q_SIGNALS:
+        void aboutToPrev();
+        void aboutToNext();
+        void finished();
+
+        void helpRequested();
+
+    protected:
+        WorkflowDialog(WorkflowDialogPrivate &d, QWidget *parent = nullptr);
+
+        QScopedPointer<WorkflowDialogPrivate> d_ptr;
+    };
+
+}
+
+#endif // CWORKFLOWDIALOG_H
