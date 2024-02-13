0f46d3c4db436e43a558efe51d50420145f5bbf0
Sine Striker <trueful@163.com>
2024-02-13 13:51:11 +0800

    Add translation

---
+++ share/translations/svscraft_ja_JP.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja_JP">
+<context>
+    <name>SVS::ConfigurableDialog</name>
+    <message>
+        <location filename="../../src/widgets/dialogs/configurabledialog.cpp" line="69"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/configurabledialog.cpp" line="70"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/configurabledialog.cpp" line="71"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SVS::WorkflowPage</name>
+    <message>
+        <location filename="../../src/widgets/dialogs/workflowpage.cpp" line="107"/>
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/workflowpage.cpp" line="108"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/workflowpage.cpp" line="109"/>
+        <source>Finish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/workflowpage.cpp" line="110"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/workflowpage.cpp" line="111"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/widgets/dialogs/workflowpage.cpp" line="112"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
