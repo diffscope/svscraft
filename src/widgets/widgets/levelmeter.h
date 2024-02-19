cbcfc63743df9568a5c15cc6dd0cea5d945165bd
CrSjimo <crs_16423@outlook.com>
2024-02-19 23:08:36 +0800

    add level meter

---
+++ src/widgets/widgets/levelmeter.h
@@ -0,0 +1,46 @@
+#ifndef LEVELMETER_H
+#define LEVELMETER_H
+
+#include <SVSCraftWidgets/svscraftwidgetsglobal.h>
+#include <QWidget>
+
+namespace SVS {
+
+    class LevelMeterPrivate;
+
+    class SVSCRAFT_WIDGETS_EXPORT LevelMeter : public QWidget {
+        Q_OBJECT
+        Q_DECLARE_PRIVATE(LevelMeter)
+    public:
+        explicit LevelMeter(QWidget *parent = nullptr);
+        ~LevelMeter() override;
+
+        QPair<bool, bool> clipped() const;
+        void resetClipIndicator();
+
+        void setValue(double valueL, double valueR);
+        QPair<double, double> value() const;
+
+        void setSafeThreshold(double threshold);
+        double safeThreshold() const;
+
+        void setWarnThreshold(double threshold);
+        double warnThreshold() const;
+
+        void setMinimum(double minimum);
+        double minimum() const;
+
+    protected:
+        void paintEvent(QPaintEvent *event) override;
+        void mousePressEvent(QMouseEvent *event) override;
+        void keyPressEvent(QKeyEvent *event) override;
+
+        LevelMeter(QWidget *parent, LevelMeterPrivate &d);
+
+    private:
+        QScopedPointer<LevelMeterPrivate> d_ptr;
+    };
+
+} // SVS
+
+#endif // LEVELMETER_H
