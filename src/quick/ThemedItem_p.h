d06dc6d2413e1c7fed58d5ae67f2a7f0e4576a63
CrSjimo <crs_16423@outlook.com>
2025-03-17 18:19:31 +0800

    add copyrights

--- src/quick/ThemedItem_p.h
+++ src/quick/ThemedItem_p.h
@@ -1,3 +1,22 @@
+/******************************************************************************
+ * Copyright (c) 2025 OpenVPI                                                 *
+ *                                                                            *
+ * This file is part of SVSCraft                                              *
+ *                                                                            *
+ * SVSCraft is free software: you can redistribute it and/or modify it under  *
+ * the terms of the GNU Lesser General Public License as published by the     *
+ * Free Software Foundation, either version 3 of the License, or (at your     *
+ * option) any later version.                                                 *
+ *                                                                            *
+ * SVSCraft is distributed in the hope that it will be useful, but WITHOUT    *
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or      *
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public        *
+ * License for more details.                                                  *
+ *                                                                            *
+ * You should have received a copy of the GNU Lesser General Public License   *
+ * along with SVSCraft. If not, see <https://www.gnu.org/licenses/>.          *
+ ******************************************************************************/
+
 #ifndef SVSCRAFT_THEMEDITEM_P_H
 #define SVSCRAFT_THEMEDITEM_P_H
 
