39ee918eb350d314f5db1190e59c34c694e9eb46
CrSjimo <crs_16423@outlook.com>
2025-03-20 18:19:16 +0800

    hard-code the default theme

--- tests/UIComponents/main.cpp
+++ tests/UIComponents/main.cpp
@@ -36,45 +36,6 @@ int main(int argc, char *argv[]) {
     sf.setSamples(8);
     QSurfaceFormat::setDefaultFormat(sf);
 
-    Theme::defaultTheme()->setAccentColor(0x5566ff);
-    Theme::defaultTheme()->setWarningColor(0xeeaa66);
-    Theme::defaultTheme()->setErrorColor(0xcc4455);
-    Theme::defaultTheme()->setButtonColor(0x333437);
-    Theme::defaultTheme()->setTextFieldColor(0x27282b);
-    Theme::defaultTheme()->setScrollBarColor(QColor::fromRgba(0x7f7f7f7f));
-    Theme::defaultTheme()->setBorderColor(0x4a4b4c);
-
-    Theme::defaultTheme()->setBackgroundPrimaryColor(0x212124);
-    Theme::defaultTheme()->setBackgroundSecondaryColor(0x232427);
-    Theme::defaultTheme()->setBackgroundTertiaryColor(0x252629);
-    Theme::defaultTheme()->setBackgroundQuaternaryColor(0x313235);
-    Theme::defaultTheme()->setSplitterColor(0x121315);
-
-    Theme::defaultTheme()->setForegroundPrimaryColor(0xdadada);
-    Theme::defaultTheme()->setForegroundSecondaryColor(QColor::fromRgba(0xa0dadada));
-
-    Theme::defaultTheme()->setLinkColor(0x5566ff);
-
-    Theme::defaultTheme()->setNavigationColor(0xffffff);
-    Theme::defaultTheme()->setShadowColor(0x101113);
-    Theme::defaultTheme()->setHighlightColor(0xb28300);
-
-    Theme::defaultTheme()->setControlDisabledColorChange({QColor::fromRgba(0x33000000)});
-    Theme::defaultTheme()->setForegroundDisabledColorChange({{}, 0.5});
-    Theme::defaultTheme()->setControlHoveredColorChange({QColor::fromRgba(0x1affffff)});
-    Theme::defaultTheme()->setForegroundHoveredColorChange({});
-    Theme::defaultTheme()->setControlPressedColorChange({});
-    Theme::defaultTheme()->setForegroundPressedColorChange({{}, 0.8});
-    Theme::defaultTheme()->setControlCheckedColorChange({QColor::fromRgba(0x1affffff)});
-
-    Theme::defaultTheme()->setAnnotationPopupTitleColorChange({{}, 0.72, 0x212124});
-    Theme::defaultTheme()->setAnnotationPopupContentColorChange({{}, 0.16, 0x212124});
-
-    Theme::defaultTheme()->setColorAnimationDuration(250);
-    Theme::defaultTheme()->setVisualEffectAnimationDuration(250);
-
-    Theme::defaultTheme()->setToolTipDelay(1000);
-
     QQuickStyle::setStyle("SVSCraft.UIComponents");
     QQuickStyle::setFallbackStyle("Basic");
 
