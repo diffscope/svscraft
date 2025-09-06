6e031399f9ab2fa8ae3bea6f8b4c97d6dc7d0d50
CrSjimo <crs_16423@outlook.com>
2025-09-07 01:21:03 +0800

    update translations

--- share/translations/svscraft_ja_JP.ts
+++ share/translations/svscraft_ja_JP.ts
@@ -1,5 +1,314 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ja_JP">
-
+<context>
+    <name>Annotation</name>
+    <message>
+        <location filename="../../src/quick/qml/controls/Annotation.qml" line="110"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AnnotationPopup</name>
+    <message>
+        <location filename="../../src/quick/qml/controls/AnnotationPopup.qml" line="184"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorComboBox</name>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorComboBox.qml" line="135"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorPalette</name>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPalette.qml" line="37"/>
+        <source>Color Palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorPicker</name>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="75"/>
+        <source>Switch palette</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="86"/>
+        <source>Show native color dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="98"/>
+        <source>Pick a color on the screen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="118"/>
+        <source>New color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="133"/>
+        <source>Current color (click to set)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="429"/>
+        <location filename="../../src/quick/qml/controls/ColorPicker.qml" line="434"/>
+        <source>Hex</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CommandPalette</name>
+    <message>
+        <location filename="../../src/quick/qml/containers/CommandPalette.qml" line="222"/>
+        <location filename="../../src/quick/qml/containers/CommandPalette.qml" line="234"/>
+        <source>Current action: </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DockingPanel</name>
+    <message>
+        <location filename="../../src/quickimpl/qml/DockingPanel.qml" line="31"/>
+        <source>Docking View Panel of %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quickimpl/qml/DockingPanel.qml" line="137"/>
+        <source>Drag to Dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quickimpl/qml/DockingPanel.qml" line="137"/>
+        <source>Undock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quickimpl/qml/DockingPanel.qml" line="137"/>
+        <source>Dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quickimpl/qml/DockingPanel.qml" line="180"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DockingView</name>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="34"/>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="268"/>
+        <source>Docking View</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="360"/>
+        <source>&amp;Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="360"/>
+        <source>&amp;Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="364"/>
+        <source>&amp;Undock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="364"/>
+        <source>&amp;Dock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="374"/>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="382"/>
+        <source>&amp;Remove from Sidebar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/containers/DockingView.qml" line="404"/>
+        <source>Show or hide the docking pane</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GroupBox</name>
+    <message>
+        <location filename="../../src/quick/qml/quickcontrols/GroupBox.qml" line="66"/>
+        <source>Expand</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/quickcontrols/GroupBox.qml" line="66"/>
+        <source>Collapse</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MessageBoxDialog</name>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="164"/>
+        <source>Hide Details...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="164"/>
+        <source>Show Details...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="197"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="254"/>
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="200"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="256"/>
+        <source>Restore Defaults</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="203"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="258"/>
+        <source>&amp;Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="206"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="260"/>
+        <source>Yes to &amp;All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="209"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="262"/>
+        <source>OK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="212"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="264"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="215"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="266"/>
+        <source>Save All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="218"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="268"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="221"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="270"/>
+        <source>Retry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="224"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="272"/>
+        <source>Ignore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="227"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="274"/>
+        <source>Discard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="230"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="276"/>
+        <source>&amp;No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="233"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="278"/>
+        <source>N&amp;o to All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="236"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="280"/>
+        <source>Abort</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="239"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="282"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="242"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="284"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="245"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="286"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="248"/>
+        <location filename="../../src/quick/qml/MessageBoxDialog.qml" line="288"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SystemButton</name>
+    <message>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="27"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="29"/>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="31"/>
+        <source>Maximize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="29"/>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="33"/>
+        <source>Restore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="35"/>
+        <source>Minimize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/quick/qml/controls/SystemButton.qml" line="37"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
