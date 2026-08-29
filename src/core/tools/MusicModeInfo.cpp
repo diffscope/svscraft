601b0f018b851e49d4b4cbbe53ddbd69f3e4988d
CrSjimo <crs_16423@outlook.com>
2026-08-30 05:27:47 +0800

    Fix Qt translate

--- src/core/tools/MusicModeInfo.cpp
+++ src/core/tools/MusicModeInfo.cpp
@@ -23,28 +23,45 @@
 
 namespace SVS {
 
-    constexpr MusicMode m(std::initializer_list<int> scale) {
-        int mask = 0;
-        for (int i : scale) {
-            mask |= 1 << i;
+    namespace {
+        constexpr MusicMode m(std::initializer_list<int> scale) {
+            int mask = 0;
+            for (int i : scale) {
+                mask |= 1 << i;
+            }
+            return MusicMode(mask);
         }
-        return MusicMode(mask);
+
+        constexpr char majorModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Major");
+        constexpr char minorModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Minor");
+        constexpr char harmonicMajorModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Harmonic Major");
+        constexpr char harmonicMinorModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Harmonic Minor");
+        constexpr char melodicMajorModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Melodic Major");
+        constexpr char melodicMinorModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Melodic Minor");
+        constexpr char dorianModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Dorian");
+        constexpr char phrygianModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Phrygian");
+        constexpr char lydianModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Lydian");
+        constexpr char mixolydianModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Mixolydian");
+        constexpr char locrianModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Locrian");
+        constexpr char atonalModeName[] = QT_TRANSLATE_NOOP("SVS::MusicModeInfo", "Atonal");
+
     }
+
 
     QList<MusicModeInfo> MusicModeInfo::getBuiltInMusicModeInfoList() {
         return {
-            {m({0, 2, 4, 5, 7, 9, 11}), QCoreApplication::translate("SVS::MusicModeInfo", "Major")},
-            {m({0, 2, 3, 5, 7, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", "Minor")},
-            {m({0, 2, 4, 5, 7, 8, 11}), QCoreApplication::translate("SVS::MusicModeInfo", "Harmonic Major")},
-            {m({0, 2, 3, 5, 7, 8, 11}), QCoreApplication::translate("SVS::MusicModeInfo", "Harmonic Minor")},
-            {m({0, 2, 4, 5, 7, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", "Melodic Major")},
-            {m({0, 2, 3, 5, 7, 9, 11}), QCoreApplication::translate("SVS::MusicModeInfo", "Melodic Minor")},
-            {m({0, 2, 3, 5, 7, 9, 10}), QCoreApplication::translate("SVS::MusicModeInfo", "Dorian")},
-            {m({0, 1, 3, 5, 7, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", "Phrygian")},
-            {m({0, 2, 4, 6, 7, 9, 11}), QCoreApplication::translate("SVS::MusicModeInfo", "Lydian")},
-            {m({0, 2, 4, 5, 7, 9, 10}), QCoreApplication::translate("SVS::MusicModeInfo", "Mixolydian")},
-            {m({0, 1, 3, 5, 6, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", "Locrian")},
-            {MusicMode(0), QCoreApplication::translate("SVS::MusicModeInfo", "Atonal")},
+            {m({0, 2, 4, 5, 7, 9, 11}), QCoreApplication::translate("SVS::MusicModeInfo", majorModeName)},
+            {m({0, 2, 3, 5, 7, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", minorModeName)},
+            {m({0, 2, 4, 5, 7, 8, 11}), QCoreApplication::translate("SVS::MusicModeInfo", harmonicMajorModeName)},
+            {m({0, 2, 3, 5, 7, 8, 11}), QCoreApplication::translate("SVS::MusicModeInfo", harmonicMinorModeName)},
+            {m({0, 2, 4, 5, 7, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", melodicMajorModeName)},
+            {m({0, 2, 3, 5, 7, 9, 11}), QCoreApplication::translate("SVS::MusicModeInfo", melodicMinorModeName)},
+            {m({0, 2, 3, 5, 7, 9, 10}), QCoreApplication::translate("SVS::MusicModeInfo", dorianModeName)},
+            {m({0, 1, 3, 5, 7, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", phrygianModeName)},
+            {m({0, 2, 4, 6, 7, 9, 11}), QCoreApplication::translate("SVS::MusicModeInfo", lydianModeName)},
+            {m({0, 2, 4, 5, 7, 9, 10}), QCoreApplication::translate("SVS::MusicModeInfo", mixolydianModeName)},
+            {m({0, 1, 3, 5, 6, 8, 10}), QCoreApplication::translate("SVS::MusicModeInfo", locrianModeName)},
+            {MusicMode(0), QCoreApplication::translate("SVS::MusicModeInfo", atonalModeName)},
         };
     }
 
